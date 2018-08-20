80cfbf33ae1fac5b2c54841d8e5993f871bf1cbc
github username <github email address>
2018-08-20 18:46:44 -0500

    links

--- app/controllers/victims_controller.rb
+++ app/controllers/victims_controller.rb
@@ -1,5 +1,5 @@
 class VictimsController < ApplicationController
-  before_action :set_victim, only: [:show, :edit, :update, :destroy]
+  before_action :set_victim, only: [:show]
 
   def index
     @victims = Victim.all
