bbf630c84eea2176ef998755e56849e6aa056fab
github username <github email address>
2018-08-22 09:20:40 -0500

    mailer stuff

--- app/mailers/spam_email_mailer.rb
+++ app/mailers/spam_email_mailer.rb
@@ -1,8 +1,8 @@
 class SpamEmailMailer < ApplicationMailer
   default from: 'test@example.com'
 
-  def send_spam_email
-    @url  = 'http://example.com/login'
-    mail(to: params[:victim][:real_email], subject: "test", template_path: 'spam_emails', template_name: 'confirm')
+  def punch1(victim)
+    @victim = victim
+    mail(to: params[:victim][:real_email], subject: "test")
   end
 end
